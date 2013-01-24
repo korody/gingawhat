c29ea5c46d63643b8ba99fb26029853bb3ea8fef
korody <marcoskorody@hotmail.com>
2013-01-24 01:51:36 -0200

    Temp Background

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "ginga WHAT?"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
