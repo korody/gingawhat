e364d78c6c00206c8285248437e7bc5a3898c74a
korody <marcoskorody@hotmail.com>
2013-01-24 01:04:06 -0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gingawhat::Application.config.secret_token = 'a512b88e217f83b4e281cab183603377adc7eed4787fb9e48253b98fe740072098d24c27b478e69123b3bcb5d7c0459d33b2c496718183249f87899025c268c6'
